6b424b56f9a3e0fe4960b514753d77f625ceee4a
Reihan Wiyanda <Reihan Wiyanda@Mancungs>
2021-02-20 13:23:42 +0700

    UAS-IF1-10119011-REIHANWIYANDA-2

---
+++ src/uas/if1/pkg10119011/reihanwiyanda/pkg2/UASIF110119011REIHANWIYANDA2.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uas.if1.pkg10119011.reihanwiyanda.pkg2;
+
+/**
+ *
+ * @author 
+ *  Nama    : Reihan Wiyanda
+ *  Kelas   : IF-1
+ *  Nim     : 10119011
+ */
+public class UASIF110119011REIHANWIYANDA2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int saldoawal = 100000;
+//        int bulan = 0;
+        int saldo;
+        
+        for (int bulan = 0; bulan<=5; saldoawal+=500 ) {
+//            bulan = bulan + 1;
+//            saldo = saldoawal + 5000;
+            System.out.println("Saldo di bulan ke-" + bulan + " : Rp" + saldoawal);
+        }
+    }
+    
+}
